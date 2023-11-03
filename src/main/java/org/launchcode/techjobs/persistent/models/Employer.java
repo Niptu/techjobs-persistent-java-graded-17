439d8451b4980f55ad025b544a0b291c9e33251c
Nikolai <nikolaiptukhin@gmail.com>
2023-11-02 23:58:14 -0400

    Stopped on Task 2 Controllers

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(max = 100)
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.validation.constraints.Size;
+
 public class Skill extends AbstractEntity {
 
+    @Size(max = 300)
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
